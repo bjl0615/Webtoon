2b537e81288d3947b6c0da31f2f5c87bc36e190c
admin <bjl0615@gmail.com>
2023-08-01 23:29:55 +0900

    Add Fragment

--- app/src/main/java/com/example/webtoon/MainActivity.kt
+++ app/src/main/java/com/example/webtoon/MainActivity.kt
@@ -4,16 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.example.webtoon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val webView = findViewById<WebView>(R.id.webView)
-        webView.webViewClient = WebViewClient()
-        webView.settings.javaScriptEnabled = true
+        val container = binding.fragmentContainer
 
-        webView.loadUrl("https://google.com")
+        binding.button1.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, WebViewFragment())
+                commit()
+            }
+        }
+
+        binding.button2.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, BFragment())
+                commit()
+            }
+        }
+
+
+
+
     }
 }
